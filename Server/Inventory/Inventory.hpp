832f26701a13ee0c3be9eaa1159f418f4ec9112f
Valiance <lucas.ludovic@epitech.eu>
2025-05-26 15:32:29 +0200

    feat: add inventory and item class

---
+++ Server/Inventory/Inventory.hpp
@@ -0,0 +1,38 @@
+//
+// EPITECH PROJECT, 2025
+// Player Inventory
+// File description:
+// Player Inventory
+//
+
+#pragma once
+
+#include <cstdint>
+#include <map>
+
+#define NO_ITEM 0
+
+namespace ZappyPlayer {
+    enum class Item : uint8_t {
+        food,
+        linemate,
+        deraumere,
+        sibur,
+        mendiane,
+        phiras,
+        thrystame
+    };
+
+    class Inventory {
+       public:
+        Inventory();
+        ~Inventory() = default;
+
+        void addItem(const Item &item);
+        [[nodiscard]] bool removeItem(const Item &item);
+        void clearInventory();
+
+       private:
+        std::map<Item, size_t> _items;
+    };
+}  // namespace ZappyPlayer
