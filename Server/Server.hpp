c6e88e7d84e36c6f2abb58b5a1a05e92df95f719
Baptiste Blambert Deminière <baptiste.blambert@epitech.eu>
2025-05-26 16:06:08 +0200

    add: Server Parsing

--- Server/Server.hpp
+++ Server/Server.hpp
@@ -12,21 +12,38 @@
 #include <netinet/in.h>
 #include <poll.h>
 #include <string>
+#include <iostream>
+#include <sstream>
 
+#include "Error.hpp"
 #include "Player.hpp"
 
 namespace ZappyServer {
 
     class Server {
         public:
-            Server();
-            ~Server();
+            Server() : _port(-1), _width(-1), _height(-1), _clientNb(-1), _freq(-1) {}
+            ~Server() = default;
+
+            void parsing(int argc, char const *argv[]);
+            void parsingName(int &index, char const *argv[]);
+
+            const int getWidth() { return _width; }
+            const int getHeight() { return _height; }
+            const int getClientNb() { return _clientNb; }
 
         private:
-            std::size_t _port;
-            int servSocket;
+
+            int _port;
+            int _servSocket;
             sockaddr_in servAddr{};
-            std::vector<Team> _clientList;
+            std::vector<ZappyPlayer::Team> _clientList;
             std::vector<pollfd> fds;
+
+            int _width;
+            int _height;
+            int _clientNb;
+            int _freq;
+            std::vector<std::string> _namesTeam;
     };
 }
