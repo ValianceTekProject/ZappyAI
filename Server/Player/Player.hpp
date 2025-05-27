5d77be2ae02a8e716d0a0a694e3c044bdc81577c
Baptiste Blambert Deminière <baptiste.blambert@epitech.eu>
2025-05-27 15:43:47 +0200

    feat: connexion with client and chosing team

--- Server/Player/Player.hpp
+++ Server/Player/Player.hpp
@@ -12,18 +12,32 @@
 #include <vector>
 
 namespace ZappyPlayer {
+
+    enum class ClientState {
+        WAITING_TEAM_NAME,
+        CONNECTED,
+    };
+
     class User {
        public:
-        User();
-        ~User();
+        User() : _state(ClientState::WAITING_TEAM_NAME) {};
+        ~User() = default;
+
+        int getSocket() const { return _socket; }
+        void setSocket(int socket) { _socket = socket; }
+
+        ClientState getState() const { return _state; }
+        void setState(ClientState state) { _state = state; }
 
        private:
+        int _socket;
+        ClientState _state;
     };
 
     class Team {
        public:
-        Team();
-        ~Team();
+        Team() = default;
+        ~Team() = default;
 
         std::string getName() const { return _name; }
 
@@ -32,6 +46,10 @@ namespace ZappyPlayer {
         std::size_t getSize() const { return _size; }
 
         void setSize(const std::size_t size) { this->_size = size; }
+
+        std::size_t getSizeMax() const { return _sizeMax; }
+
+        void setSizeMax(const std::size_t size) { this->_sizeMax = size; }
 
         const std::vector<std::reference_wrapper<User>> getUserList() const
         {
@@ -43,6 +61,7 @@ namespace ZappyPlayer {
        private:
         std::string _name;
         std::size_t _size;
+        std::size_t _sizeMax;
         std::vector<std::reference_wrapper<User>> _userList;
     };
-}  // namespace ZappyPlayer
+}
--- Server/Server.hpp
+++ Server/Server.hpp
@@ -14,31 +14,52 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <csignal>
+#include <algorithm>
 
 #include "Error.hpp"
 #include "Player.hpp"
 
 namespace ZappyServer {
 
+    #define OK 0
+    #define KO 84
+
     class Server {
         public:
-            Server() : _port(-1), _width(-1), _height(-1), _clientNb(-1), _freq(-1) {}
+            Server() : _serverRun(true), _port(-1), _width(-1), _height(-1), _clientNb(-1), _freq(-1) {}
             ~Server() = default;
+
+            void serverLaunch();
 
             void parsing(int argc, char const *argv[]);
             void parsingName(int &index, char const *argv[]);
 
-            const int getWidth() { return _width; }
-            const int getHeight() { return _height; }
-            const int getClientNb() { return _clientNb; }
+            int getWidth() const { return _width; }
+            int getHeight() const { return _height; }
+            int getClientNb() const { return _clientNb; }
+
+            void serverLoop();
+            void handleNewConnection();
+            void handleTeamJoin(int clientSocket, const std::string &teamName);
+
+            void stopServer(int sig);
+            void closeClients();
+            static void signalWrapper(int sig);
+            static std::function<void(int)> takeSignal;
+
+            void sendMessage(int clientSocket, char const *message) { write(clientSocket, message, std::string(message).size()); }
 
         private:
 
+            bool _serverRun;
             int _port;
             int _servSocket;
             sockaddr_in servAddr{};
-            std::vector<ZappyPlayer::Team> _clientList;
+            std::vector<ZappyPlayer::Team> _teamList;
             std::vector<pollfd> fds;
+
+            std::unordered_map<int, ZappyPlayer::User> _users;
 
             int _width;
             int _height;
@@ -46,4 +67,12 @@ namespace ZappyServer {
             int _freq;
             std::vector<std::string> _namesTeam;
     };
+
+    // Encapsuled Functions
+    int my_socket(int __domain, int __type, int __protocol);
+    int my_bind(int __fd, const sockaddr *__addr, socklen_t __len);
+    int my_listen(int __fd, int __n);
+    int my_poll(pollfd *__fds, nfds_t __nfds, int __timeout);
+    int my_accept(int __fd, sockaddr *__restrict__ __addr, socklen_t *__restrict__ __addr_len);
+    sighandler_t my_signal(int __sig, sighandler_t __handler);
 }
