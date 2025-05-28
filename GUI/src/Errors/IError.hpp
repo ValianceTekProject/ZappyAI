9e70320a519c486b368ed8047bc345778e4f2ab5
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

--- GUI/src/Errors/IError.hpp
+++ GUI/src/Errors/IError.hpp
@@ -1,10 +1,6 @@
 /*
 ** EPITECH PROJECT, 2025
-<<<<<<< HEAD
 ** B-OOP-400-BDX-4-1-zappy-baptiste.blambert
-=======
-** B-OOP-400-BDX-4-1-gui-baptiste.blambert
->>>>>>> 205ae46 (chore: architecture)
 ** File description:
 ** IError
 */
@@ -14,7 +10,6 @@
 #include <iostream>
 #include <stdexcept>
 
-<<<<<<< HEAD
 namespace zappy {
     namespace gui {
         class IError : public std::exception
@@ -24,13 +19,4 @@ namespace zappy {
                 virtual const char *where() const noexcept = 0;
         };
     }
-=======
-namespace gui {
-    class IError : public std::exception
-    {
-        public:
-            virtual const char *what() const noexcept override = 0;
-            virtual const char *where() const noexcept = 0;
-    };
->>>>>>> 205ae46 (chore: architecture)
 }
