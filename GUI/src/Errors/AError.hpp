f88946fc5e0110872256c492ac563d8993b296f0
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

--- GUI/src/Errors/AError.hpp
+++ GUI/src/Errors/AError.hpp
@@ -1,6 +1,10 @@
 /*
 ** EPITECH PROJECT, 2025
+<<<<<<< HEAD
 ** B-OOP-400-BDX-4-1-zappy-baptiste.blambert
+=======
+** B-OOP-400-BDX-4-1-gui-baptiste.blambert
+>>>>>>> 205ae46 (chore: architecture)
 ** File description:
 ** AError
 */
@@ -9,6 +13,7 @@
 
 #include "IError.hpp"
 
+<<<<<<< HEAD
 namespace zappy {
     namespace gui {
         class AError : public IError
@@ -24,4 +29,19 @@ namespace zappy {
                 std::string _where;
         };
     }
+=======
+namespace gui {
+    class AError : public IError
+    {
+        public:
+            AError(const std::string &msg, const std::string &where) noexcept;
+
+            const char *what() const noexcept;
+            const char *where() const noexcept;
+
+        private:
+            std::string _msg;
+            std::string _where;
+    };
+>>>>>>> 205ae46 (chore: architecture)
 }
