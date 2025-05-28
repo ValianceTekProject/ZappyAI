b7ed3e18a057d1cbf60481a0bdc013caea21d71b
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

---
+++ GUI/build/CMakeFiles/zappy_gui.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zappy_gui.dir/link.d"
+  "/home/axel/Epitech/Year 2/B-YEP-400/Zappy/GUI/zappy_gui"
+  "/home/axel/Epitech/Year 2/B-YEP-400/Zappy/GUI/zappy_gui.pdb"
+  "CMakeFiles/zappy_gui.dir/src/Errors/AError.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/Errors/AError.cpp.o.d"
+  "CMakeFiles/zappy_gui.dir/src/GUI.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/GUI.cpp.o.d"
+  "CMakeFiles/zappy_gui.dir/src/Game/GameMap.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/Game/GameMap.cpp.o.d"
+  "CMakeFiles/zappy_gui.dir/src/Game/GameState.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/Game/GameState.cpp.o.d"
+  "CMakeFiles/zappy_gui.dir/src/Game/Tile.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/Game/Tile.cpp.o.d"
+  "CMakeFiles/zappy_gui.dir/src/Renderer/NcursesRenderer.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/Renderer/NcursesRenderer.cpp.o.d"
+  "CMakeFiles/zappy_gui.dir/src/Renderer/OpenGLRenderer.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/Renderer/OpenGLRenderer.cpp.o.d"
+  "CMakeFiles/zappy_gui.dir/src/main.cpp.o"
+  "CMakeFiles/zappy_gui.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zappy_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
