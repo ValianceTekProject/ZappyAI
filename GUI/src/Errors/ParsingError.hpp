d0fc985ec29f13b83b8239361d240a788e891ff0
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 18:52:48 +0200

    fix: namespaces

--- GUI/src/Errors/ParsingError.hpp
+++ GUI/src/Errors/ParsingError.hpp
@@ -1,6 +1,6 @@
 /*
 ** EPITECH PROJECT, 2025
-** B-OOP-400-BDX-4-1-zappy::gui-baptiste.blambert
+** B-OOP-400-BDX-4-1-zappy-baptiste.blambert
 ** File description:
 ** ParsingError
 */
@@ -9,11 +9,13 @@
 
 #include "AError.hpp"
 
-namespace zappy::gui {
+namespace zappy {
+    namespace gui {
     class ParsingError : public AError
-    {
-        public:
-            ParsingError(const std::string &msg, const std::string &where)
-                noexcept : AError(msg, where) {};
-    };
+        {
+            public:
+                ParsingError(const std::string &msg, const std::string &where)
+                    noexcept : AError(msg, where) {};
+        };
+    }
 }
