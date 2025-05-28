e99b318d9193000713d86167685b698ee1cebdd1
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

---
+++ GUI/src/Errors/AError.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-BDX-4-1-gui-baptiste.blambert
+** File description:
+** AError
+*/
+
+#pragma once
+
+#include "IError.hpp"
+
+namespace gui {
+    class AError : public IError
+    {
+        public:
+            AError(const std::string &msg, const std::string &where) noexcept;
+
+            const char *what() const noexcept;
+            const char *where() const noexcept;
+
+        private:
+            std::string _msg;
+            std::string _where;
+    };
+}
---
+++ GUI/src/Errors/GUIError.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-BDX-4-1-gui-baptiste.blambert
+** File description:
+** GUIError
+*/
+
+#pragma once
+
+#include "AError.hpp"
+
+namespace gui {
+    class GUIError : public AError
+    {
+        public:
+            GUIError(const std::string &msg, const std::string &where)
+                noexcept : AError(msg, where) {};
+    };
+}
