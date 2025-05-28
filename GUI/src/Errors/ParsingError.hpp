698c61d8b347c56076ada5bc31bcf98538545c71
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

--- GUI/src/Errors/ParsingError.hpp
+++ GUI/src/Errors/ParsingError.hpp
@@ -1,6 +1,10 @@
 /*
 ** EPITECH PROJECT, 2025
+<<<<<<< HEAD
 ** B-OOP-400-BDX-4-1-zappy-baptiste.blambert
+=======
+** B-OOP-400-BDX-4-1-gui-baptiste.blambert
+>>>>>>> 205ae46 (chore: architecture)
 ** File description:
 ** ParsingError
 */
@@ -9,6 +13,7 @@
 
 #include "AError.hpp"
 
+<<<<<<< HEAD
 namespace zappy {
     namespace gui {
     class ParsingError : public AError
@@ -18,4 +23,13 @@ namespace zappy {
                     noexcept : AError(msg, where) {};
         };
     }
+=======
+namespace gui {
+    class ParsingError : public AError
+    {
+        public:
+            ParsingError(const std::string &msg, const std::string &where)
+                noexcept : AError(msg, where) {};
+    };
+>>>>>>> 205ae46 (chore: architecture)
 }
