0a23f0fb2ce84914f594a72b8d2226685284da24
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

---
+++ GUI/src/Renderer/OpenGLRenderer.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** OpenGLRenderer.cpp
+*/
+
+#pragma once
+
+#include "IRenderer.hpp"
+
+#include <GL/glew.h>
+
+namespace gui {
+    class OpenGLRenderer : public IRenderer
+    {
+        public:
+            OpenGLRenderer();
+            ~OpenGLRenderer();
+
+
+    };
+}
