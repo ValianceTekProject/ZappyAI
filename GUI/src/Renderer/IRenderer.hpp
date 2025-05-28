e0444e2ca755606eb2e00e75ca9725a5309dc727
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

---
+++ GUI/src/Renderer/IRenderer.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** IRenderer.hpp
+*/
+
+namespace gui {
+    class IRenderer
+    {
+        public:
+            virtual ~IRenderer() = default;
+
+            virtual void init() = 0;
+
+            virtual void handleInput() = 0;
+            virtual void update() = 0;
+
+            virtual void render() = 0;
+
+            virtual void close() = 0;
+    };
+}
