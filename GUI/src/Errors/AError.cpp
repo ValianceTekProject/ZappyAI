0dad1ca5ebc83da9a0035a1ff8c2885da4de6435
Oursegamin <axel.lavrador@gmail.com>
2025-05-28 10:29:47 +0200

    chore: architecture

--- GUI/src/Errors/AError.cpp
+++ GUI/src/Errors/AError.cpp
@@ -1,23 +1,39 @@
 /*
 ** EPITECH PROJECT, 2025
+<<<<<<< HEAD
 ** B-OOP-400-BDX-4-1-zappy-baptiste.blambert
+=======
+** B-OOP-400-BDX-4-1-gui-baptiste.blambert
+>>>>>>> 205ae46 (chore: architecture)
 ** File description:
 ** AError
 */
 
 #include "AError.hpp"
 
+<<<<<<< HEAD
 zappy::gui::AError::AError(const std::string &msg,
+=======
+gui::AError::AError(const std::string &msg,
+>>>>>>> 205ae46 (chore: architecture)
     const std::string &where) noexcept :
     _msg(msg),
     _where(where) {}
 
+<<<<<<< HEAD
 const char *zappy::gui::AError::what() const noexcept
+=======
+const char *gui::AError::what() const noexcept
+>>>>>>> 205ae46 (chore: architecture)
 {
     return _msg.c_str();
 }
 
+<<<<<<< HEAD
 const char *zappy::gui::AError::where() const noexcept
+=======
+const char *gui::AError::where() const noexcept
+>>>>>>> 205ae46 (chore: architecture)
 {
     return _where.c_str();
 }
--- GUI/src/Errors/IError.hpp
+++ GUI/src/Errors/IError.hpp
@@ -1,6 +1,10 @@
 /*
 ** EPITECH PROJECT, 2025
+<<<<<<< HEAD
 ** B-OOP-400-BDX-4-1-zappy-baptiste.blambert
+=======
+** B-OOP-400-BDX-4-1-gui-baptiste.blambert
+>>>>>>> 205ae46 (chore: architecture)
 ** File description:
 ** IError
 */
@@ -10,6 +14,7 @@
 #include <iostream>
 #include <stdexcept>
 
+<<<<<<< HEAD
 namespace zappy {
     namespace gui {
         class IError : public std::exception
@@ -19,4 +24,13 @@ namespace zappy {
                 virtual const char *where() const noexcept = 0;
         };
     }
+=======
+namespace gui {
+    class IError : public std::exception
+    {
+        public:
+            virtual const char *what() const noexcept override = 0;
+            virtual const char *where() const noexcept = 0;
+    };
+>>>>>>> 205ae46 (chore: architecture)
 }
