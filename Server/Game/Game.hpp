6ce8dc2318f9b45a547d7050efe5664dd79518a6
Baptiste Blambert Deminière <baptiste.blambert@epitech.eu>
2025-05-28 15:19:54 +0200

    fix: team architecture

---
+++ Server/Game/Game.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** Game
+*/
+
+#pragma once
+
+namespace zappy {
+    namespace game {
+        class Game
+        {
+            public:
+
+                Game() = default;
+                ~Game() = default;
+
+
+            private:
+                
+        };
+        
+        
+    }
+}
