8277fdf4a1b056639813e97e5ced95eae5c698c7
Oursegamin <axel.lavrador@gmail.com>
2025-05-29 12:57:02 +0200

    feat: argument parsing

--- GUI/src/Game/GameState.hpp
+++ GUI/src/Game/GameState.hpp
@@ -14,11 +14,18 @@ namespace zappy {
         class GameState
         {
             public:
-                GameState() = default;
+                GameState() : _frequency(100) {}
                 ~GameState() = default;
 
+                void setFrequency(size_t frequency) { _frequency = frequency; }
+                void initMap(size_t width, size_t height) { _map.init(width, height); }
+
+                size_t getFrequency() const { return _frequency; }
+
             private:
-                game::Map _map;
+            size_t _frequency;
+
+            game::Map _map;
         };
     }
 }
