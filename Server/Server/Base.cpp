8a4c71a08de17da48cb485efe8727263e98a25ee
Valiance <lucas.ludovic@epitech.eu>
2025-06-03 22:14:14 +0200

    refacto: refacto server init

--- Server/Server/Base.cpp
+++ Server/Server/Base.cpp
@@ -6,96 +6,93 @@
 */
 
 #include "Server.hpp"
-#include "EncapsuledFunction/Socket.hpp"
+#include "Socket.hpp"
 #include "Error/Error.hpp"
 #include "Game.hpp"
+#include "my_macros.hpp"
 #include <memory>
 
 zappy::server::Server::Server(int argc, char const *argv[])
 {
-    this->_game = std::make_unique<zappy::game::Game>();
-    this->_parseArgs(argc, argv);
+    this->_flags = {
+        {"-p", [this](int value) {this->_port = value;}},
+        {"-x", [this](int value) {this->_width = value;}},
+        {"-y", [this](int value) {this->_height = value;}},
+        {"-c", [this](int value) {this->_clientNb = value;}},
+        {"-f", [this](int value) {this->_freq = value;}}
+    };
+    this->_parseFlags(argc, argv);
+    this->_game = std::make_unique<zappy::game::Game>(this->_width, this->_height);
+    this->_socket =
+        std::make_unique<server::Socket>(this->_port, this->_clientNb);
+    this->_fds.push_back({this->_socket->getSocket(), POLLIN, 0});
+    std::cout << "Zappy Server listening on port " << this->_port << "...\n";
 }
 
-int handlerFlag(char const *argv[], int i, std::string flag)
+int handleFlag(const std::string &flag)
 {
     std::stringstream stream;
-    int res = -1;
+    int res;
 
-    if (std::string(argv[i]) == flag && argv[i + 1]) {
-        stream << std::string(argv[i + 1]);
-        stream >> res;
-    }
+    stream << flag;
+    stream >> res;
     return res;
 }
 
 void zappy::server::Server::_parseName(int &index, char const *argv[])
 {
-    int nameCount = 0;
-
+    index += 1;
     while (argv[index]) {
         if (std::string(argv[index]).find("-") == 0)
             break;
-        _namesTeam.push_back(argv[index]);
-        nameCount += 1;
+        this->_namesTeam.push_back(argv[index]);
         index += 1;
         zappy::game::Team team(argv[index]);
-        _teamList.push_back(team);
+        this->_teamList.push_back(team);
     }
 
-    if (nameCount == 0)
+    if (this->_teamList.empty())
         throw error::InvalidArg("Flag -n must have at least on argument !");
+    index -= 1;
 }
 
-void zappy::server::Server::_parseArgs(int argc, char const *argv[])
+void zappy::server::Server::_parseFlagsInt(int &index, std::string arg, std::string value)
+{
+    auto flagsIt = this->_flags.find(arg);
+    if (flagsIt == this->_flags.end())
+        throw error::Error("Missing or invalid value");
+    auto valueInt = handleFlag(value);
+    flagsIt->second(valueInt);
+    index += 1;
+}
+
+void zappy::server::Server::_parseFlags(int argc, char const *argv[])
 {
     if (argv == nullptr || argv[0] == nullptr)
         throw error::ServerConnection("Unable to access argv");
-    for (int i = 1; i < argc; ++i) {
-        if (!argv[i])
+
+    for (int i = 1; i < argc; i += 1) {
+        if (argv[i] == nullptr)
             throw error::Error("Error in argument list");
 
         std::string currentArg = argv[i];
-
         if (currentArg == "-n") {
-            ++i;
-            _parseName(i, argv);
-            --i;
+            this->_parseName(i, argv);
             continue;
         }
-
-        int value = handlerFlag(argv, i, currentArg);
-        if (value != -1) {
-            if (currentArg == "-p")
-                _port = value;
-            else if (currentArg == "-x")
-                _width = value;
-            else if (currentArg == "-y")
-                _height = value;
-            else if (currentArg == "-c")
-                _clientNb = value;
-            else if (currentArg == "-f")
-                _freq = value;
-            i += 1;
-        } else
-            throw error::InvalidArg(
-                "Unknown or badly formatted argument: " + currentArg);
+        this->_parseFlagsInt(i, currentArg, argv[i + 1]);
     }
 
-    if (_port == -1 || _width == -1 || _height == -1 || _clientNb == -1 ||
-        _freq == -1 || _namesTeam.empty())
+    if (this->_port == zappy::noValue || this->_width == zappy::noValue ||
+        this->_height == zappy::noValue || this->_clientNb == zappy::noValue ||
+        this->_freq == zappy::noValue || this->_namesTeam.empty()) {
         throw error::InvalidArg(
             "Missing arguments: -p -x -y -c -f -n <names>");
+    }
 }
 
 void zappy::server::Server::runServer()
 {
-    this->_socket =
-        std::make_unique<server::Socket>(this->_port, this->_clientNb);
-
-    std::cout << "Zappy Server listening on port " << this->_port << "...\n";
-    fds.push_back({this->_socket->getSocket(), POLLIN, 0});
-
     std::thread networkThread(&zappy::server::Server::runLoop, this);
     std::thread gameThread(&game::Game::gameLoop, this->_game.get());
 
