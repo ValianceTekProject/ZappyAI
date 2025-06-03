c90d26391a35f9b39c4dc4ac064f13d72a7335fc
Valiance <lucas.ludovic@epitech.eu>
2025-06-03 16:17:12 +0200

    refacto: remove magic number in game and map

--- Server/Game/Game.hpp
+++ Server/Game/Game.hpp
@@ -7,29 +7,19 @@
 
 #pragma once
 
-#include "Teams/Teams.hpp"
 #include "Map/Map.hpp"
-#include "my_macros.hpp"
-#include "Commands/ClientCommand.hpp"
 #include <atomic>
 #include <chrono>
 
 namespace zappy {
     namespace game {
 
-        // constexpr int baseGameFreqMs = 1000;
+        constexpr int baseGameFreqMs = 1000;
 
         class Game {
 
            public:
-            Game(int mapWidth, int mapHeight, std::vector<Team> teamList, int freq, int clientNb)
-                : _map(mapWidth, mapHeight),
-                _commandHandler(freq),
-                _teamList(std::move(teamList)),
-                _baseFreqMs(freq),
-                _clientNb(clientNb)
-            {}
-
+            Game() : _baseFreqMs(baseGameFreqMs) {}
 
             ~Game() = default;
 
@@ -39,18 +29,13 @@ namespace zappy {
             bool handleTeamJoin(int clientSocket, const std::string &teamName);
             void removeFromTeam(int clientSocket);
 
-            int getFreq() { return this->_baseFreqMs; }
-            MapServer &getMap() { return this->_map; }
-            std::vector<zappy::game::Team> &getTeamList() { return this->_teamList; };
+            void setRun(bool run) { this->_isRunning = run; };
 
            private:
             int _idTot = 0;
             MapServer _map;
-            zappy::game::CommandHandler _commandHandler;
-            std::vector<zappy::game::Team> _teamList;
-            int _baseFreqMs;
-            int _clientNb;
-            std::atomic<RunningState> _isRunning = RunningState::PAUSE;
+            std::chrono::milliseconds _baseFreqMs;
+            std::atomic<bool> _isRunning = false;
 
             void _playTurn();
             bool _checkAlreadyInTeam(int clientSocket);
