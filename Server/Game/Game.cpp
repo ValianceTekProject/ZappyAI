f0b013e4d7b68883759945e6faf69ff86dd3f0c9
Valiance <lucas.ludovic@epitech.eu>
2025-06-03 15:50:29 +0200

    refacto: split socket init into subfunctions

--- Server/Game/Game.cpp
+++ Server/Game/Game.cpp
@@ -5,8 +5,7 @@
 ** Game
 */
 
-#include "Game.hpp"
-#include "Player/Player.hpp"
+#include "Game.hpp" #include "Data/Game/Resource.hpp"
 #include "Server.hpp"
 #include "Teams/Teams.hpp"
 #include <algorithm>
