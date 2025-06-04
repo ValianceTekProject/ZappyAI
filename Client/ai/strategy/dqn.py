5505b0e84c9856c7d761fff4a24517681c70bc23
Yuuko <tilio.huart@epitech.eu>
2025-06-04 08:53:50 +0200

    feat (DQN): initialize DQN class

---
+++ Client/ai/strategy/dqn.py
@@ -0,0 +1,12 @@
+##
+## EPITECH PROJECT, 2025
+## Zappy
+## File description:
+## dqn
+##
+
+from typing import List, Dict, Any, Optional, Tuple
+
+class DeepQNetwork:
+    def __init__(self, state: Dict[str, Any], position: Tuple[int, int]):
+        pass
