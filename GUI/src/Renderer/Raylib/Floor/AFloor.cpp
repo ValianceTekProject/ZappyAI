fd3e6a79b65a57fa9b7aae0672013f52b415820a
Simon Maigrot <simon.maigrot33@gmail.com>
2025-06-13 17:58:48 +0200

    fix: Fixing AFloor to include a reference to map

--- GUI/src/Renderer/Raylib/Floor/AFloor.cpp
+++ GUI/src/Renderer/Raylib/Floor/AFloor.cpp
@@ -7,15 +7,7 @@
 
 #include "AFloor.hpp"
 
-zappy::gui::raylib::AFloor::AFloor()
-    : _gridSize(10), _spacing(5) {}
-
 // Setters
-void zappy::gui::raylib::AFloor::setGridSize(int size)
-{
-    _gridSize = size;
-}
-
 void zappy::gui::raylib::AFloor::setSpacing(int spacing)
 {
     _spacing = spacing;
@@ -32,11 +24,6 @@ void zappy::gui::raylib::AFloor::setHeight(int height)
 }
 
 // Getters
-int zappy::gui::raylib::AFloor::getGridSize() const
-{
-    return _gridSize;
-}
-
 int zappy::gui::raylib::AFloor::getSpacing() const
 {
     return _spacing;
@@ -46,7 +33,10 @@ int zappy::gui::raylib::AFloor::getWidth() const
 {
     return _width;
 }
+
 int zappy::gui::raylib::AFloor::getHeight() const
 {
     return _height;
 }
+
+void zappy::gui::raylib::AFloor::draw() const {}
