a78c2d46b4e1acbd8827bbe04982a2ec8c4a52ef
Simon Maigrot <simon.maigrot33@gmail.com>
2025-06-13 18:00:12 +0200

    add: Adding globeFunction, the same as flatFloor but in a 3d sphere

--- GUI/src/Renderer/Raylib/Floor/GlobeFloor/GlobeFloor.hpp
+++ GUI/src/Renderer/Raylib/Floor/GlobeFloor/GlobeFloor.hpp
@@ -13,10 +13,16 @@ namespace zappy {
         namespace raylib {
             class GlobeFloor : public AFloor {
                 public:
-                    GlobeFloor();
+                    GlobeFloor(const zappy::game::Map &map, int radius = 10);
                     ~GlobeFloor() = default;
 
-                    void draw(const Map &map) const override;
+                    void draw() const override;
+
+                    int getRadius() const {return _radius;}
+                    void setRadius(int radius) {_radius = radius;}
+
+                private:
+                    int _radius;
             };
         }
     } // namespace gui
