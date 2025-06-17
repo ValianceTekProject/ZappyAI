0b61bd3f488b1cf2c9bf909761b24762d7772853
Yuuko <tilio.huart@epitech.eu>
2025-06-17 09:10:14 +0200

    feat (DQN): update vision data

--- Client/ai/strategy/DQN/dqn_state.py
+++ Client/ai/strategy/DQN/dqn_state.py
@@ -7,10 +7,22 @@
 
 import numpy as np
 from utils.game_state import GameState
+from typing import Dict
+from enum import Enum
 
+class Item(Enum):
+    FOOD = 1
+    LINEMATE = 2
+    DERAUMERE = 3
+    SIBUR = 4
+    MEDIANE = 5
+    PHIRAS = 6
+    THYSTAME = 7
+    PLAYER = 8
 
 class DQNState:
-    def __init__(self, game_state: GameState, actualize_food: bool = False):
+    def __init__(self, game_state: GameState, actualize_inventory: bool = False):
+        self.game_state = game_state
         self.food_inventory = game_state.inventory.get('food', 0) / 100.0
         self.level = game_state.level / 8.0
         self.linemate_inventory = game_state.inventory.get('linemate', 0) / 20.0
@@ -20,40 +32,64 @@ class DQNState:
         self.phiras_inventory = game_state.inventory.get('phiras', 0) / 20.0
         self.thystame_inventory = game_state.inventory.get('thystame', 0) / 20.0
 
-        self.food_visible = None
-        self.distance_closest_food = None
-        self.distance_closest_linemate = None
-        if actualize_food:
-            distance_closest_food, angle = self.get_closest_food(game_state)
+        self.vision = []
+        for line in range(9):
+            tiles_in_line = 1 + (line * 2)
+            self.vision.append([self.create_empty_tile() for _ in range(tiles_in_line)])
 
-            if distance_closest_food and angle:
-                self.food_visible = 1
-                self.distance_closest_food = distance_closest_food / 20
-                self.angle_closest_food = angle / 360
-            else:
-                self.food_visible = 0
-                self.distance_closest_food = None
-                self.angle_closest_food = None
-        else:
-            self.food_visible = 0
+        if actualize_inventory:
+            self.fill_vision()
+
+    def create_empty_tile(self):
+        return {
+            Item.FOOD: 0,
+            Item.LINEMATE: 0,
+            Item.DERAUMERE: 0,
+            Item.SIBUR: 0,
+            Item.MEDIANE: 0,
+            Item.PHIRAS: 0,
+            Item.THYSTAME: 0,
+            Item.PLAYER: 0
+        }
+
+    def map_to_vision_index(self, rel_pos):
+        x, y = rel_pos
+        line = y
+        index = x + line
+        return index, line
+
+    def fill_vision(self):
+        vision = self.game_state.get_vision()
+        resources = vision.get_visible_resources()
+        for resource_name, positions_list in resources.items():
+            for position in positions_list:
+                x, y = position
+                index, line = self.map_to_vision_index((x, y))
+                item = self.convert_ressources(resource_name)
+                if item:
+                    self.vision[line][index][item] += 1
+
+    def convert_ressources(self, ressource):
+        if ressource == "food":
+            return Item.FOOD
+        if ressource == "linemate":
+            return Item.LINEMATE
+        if ressource == "deraumere":
+            return Item.DERAUMERE
+        if ressource == "sibur":
+            return Item.SIBUR
+        if ressource == "mediane":
+            return Item.MEDIANE
+        if ressource == "phiras":
+            return Item.PHIRAS
+        if ressource == "thystame":
+            return Item.THYSTAME
+        if ressource == "player":
+            return Item.PLAYER
+        return None
 
     def get_closest_food(self, game_state: GameState):
         pos = game_state.vision.find_closest_resource("food")
         if pos:
             return self.position_to_vector(pos[0], pos[1])
         return None
-
-    def position_to_vector(self, x, y):
-        distance = abs(x) + abs(y)
-
-        if distance == 0:
-            angle = 0
-        else:
-            if y < 0:
-                if x > 0:
-                    angle = 45
-                elif x < 0:
-                    angle = 315
-                else:
-                    angle = 0
-        return distance, angle
