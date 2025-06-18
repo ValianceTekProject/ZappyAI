d2aaee18770a810b006369aa1b5cb7537d536646
Baptiste Blambert Deminière <baptiste.blambert@epitech.eu>
2025-06-18 16:13:45 +0200

    fix: pop command in queue command in command manager

--- Server/Game/Game.cpp
+++ Server/Game/Game.cpp
@@ -120,7 +120,6 @@ void zappy::game::Game::runGame()
                 if (!player->getClient().queueMessage.empty()) {
                     this->_commandHandler.processClientInput(
                         player->getClient().queueMessage.front(), *player);
-                    player->getClient().queueMessage.pop();
                 }
             }
         }
