3895fe4bac9cecbbe0777cc87ef982a82832d66c
Simon Maigrot <simon.maigrot33@gmail.com>
2025-06-19 10:14:21 +0200

    feat: Adding functions in EggModel such as PlayerModel

--- GUI/src/Renderer/Raylib/Model/AEggModel.cpp
+++ GUI/src/Renderer/Raylib/Model/AEggModel.cpp
@@ -10,11 +10,12 @@
 zappy::gui::raylib::AEggModel::AEggModel(const int &id) :
     _id(id),
     _state(State::IDLE),
-    _model(),
+    _gamePosition(Vector2{0, 0}),
+    _orientation(game::Orientation::NORTH),
     _position(Vector3{0, 0, 0}),
-    _rotation(Vector3{0, 0, 0}),
+    _headOrigin(Vector3{0, 1, 0}),
     _scale(1),
-    _origin(Vector3{0, 0, 0}),
+    _model(),
     _animsCount(0),
     _animIndex(0),
     _animCurrentFrame(0),
@@ -24,4 +25,19 @@ zappy::gui::raylib::AEggModel::AEggModel(const int &id) :
 void zappy::gui::raylib::AEggModel::init()
 {
 
+}
+
+Vector3 zappy::gui::raylib::AEggModel::getHeadOrigin() const
+{
+    return Vector3{
+        _position.x + _headOrigin.x * _scale,
+        _position.y + _headOrigin.y * _scale,
+        _position.z + _headOrigin.z * _scale
+    };
+}
+
+void zappy::gui::raylib::AEggModel::rotate(const Vector3 &rotation)
+{
+    // rotate Model with rotation vector
+    (void)rotation;
 }
